f1bd39936cc7a16274b67cf297aa5c5fd1147e06
nguardadov <nguardadov@gmail.com>
2020-03-29 22:54:41 -0600

    add to logout and forgot password

--- app/src/main/java/com/guardado/loginapp/ForgotPassActivity.kt
+++ app/src/main/java/com/guardado/loginapp/ForgotPassActivity.kt
@@ -1,12 +1,21 @@
 package com.guardado.loginapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_forgot_pass.*
 
 class ForgotPassActivity : AppCompatActivity() {
 
     private lateinit var textUser: EditText
+    private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,5 +27,31 @@ class ForgotPassActivity : AppCompatActivity() {
     private fun initialise()
     {
         textUser = findViewById(R.id.txtEmail)
+        auth=FirebaseAuth.getInstance()
+        progressBar = findViewById(R.id.progressBar)
+    }
+
+    private fun send()
+    {
+        progressBar.visibility = View.VISIBLE
+        val email = txtEmail.text.toString()
+
+        if(!TextUtils.isEmpty(email))
+        {
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(this)
+                {
+                    task ->
+                    if(task.isSuccessful)
+                    {
+                        startActivity(Intent(this,LoginActivity::class.java))
+                    }else{
+                        Toast.makeText(baseContext, "Error al recuperar",
+                            Toast.LENGTH_SHORT).show()
+                        progressBar.visibility = View.GONE
+
+                    }
+                }
+        }
     }
 }
